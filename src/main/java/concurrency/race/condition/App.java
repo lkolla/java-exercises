42923746ef0f590b271d428babd73fb295b5c279
lkolla <lkolla@walmartlabs.com>
2017-12-09 23:23:59 -0800

    race condition changes

---
+++ src/main/java/concurrency/race/condition/App.java
@@ -0,0 +1,34 @@
+package concurrency.race.condition;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException{
+
+        System.out.println("Inside RaceConditionExample");
+
+        LongWrapper wrapper = new LongWrapper(0);
+
+        Runnable r = () -> {
+
+            for(int i = 0; i < 1_000; i++){
+                wrapper.incrementLong();
+            }
+        };
+
+
+        Thread[] threads = new Thread[1000];
+
+        for(int i = 0; i < 1_000; i++){
+            threads[i] = new Thread(r);
+            threads[i].start();
+        }
+
+        for(int i = 0; i < 1_000; i++){
+            threads[i].join();
+        }
+
+        System.out.println(wrapper.getLong());
+
+
+    }
+
+}
---
+++ src/main/java/concurrency/race/condition/LongWrapper.java
@@ -0,0 +1,25 @@
+package concurrency.race.condition;
+
+public class LongWrapper {
+
+    private Object key = new Object();
+
+    private long l;
+
+    public LongWrapper(long l){
+        this.l = l;
+    }
+
+    public long getLong(){
+        return l;
+    }
+
+    public void incrementLong(){
+
+        synchronized (key){
+            l = l + 1;
+        }
+
+    }
+
+}
