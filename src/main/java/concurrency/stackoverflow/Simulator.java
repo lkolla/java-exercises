d1c221b3819d5431fc8f33c81b698f2f2b57bceb
lkolla <lkolla@walmartlabs.com>
2017-12-10 00:02:47 -0800

    added stackoverflow simulator.

---
+++ src/main/java/concurrency/stackoverflow/Simulator.java
@@ -0,0 +1,17 @@
+package concurrency.stackoverflow;
+
+public class Simulator {
+
+    public static void recursivePrint(int num) {
+        System.out.println("Number: " + num);
+
+        if(num == 0)
+            return;
+        else
+            recursivePrint(++num);
+    }
+
+    public static void main(String[] args) {
+        Simulator.recursivePrint(1);
+    }
+}
